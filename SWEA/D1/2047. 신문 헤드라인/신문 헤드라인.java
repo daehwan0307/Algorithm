d9f57889e51e08d263b4e237733d776fdebfafc7
DaeHwan Jung <50093472+daehwan0307@users.noreply.github.com>
2022-10-27 21:35:11 +0900

    [D1] Title: 신문 헤드라인, Time: 121 ms, Memory: 29,308 KB -BaekjoonHub

--- SWEA/D1/2047. 신문 헤드라인/신문 헤드라인.java
+++ SWEA/D1/2047. 신문 헤드라인/신문 헤드라인.java
@@ -27,21 +27,20 @@
 /////////////////////////////////////////////////////////////////////////////////////////////
 import java.util.Scanner;
 import java.io.FileInputStream;
-import java.util.Locale;
 /*
    사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
    이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
  */
 class Solution
 {
-    public static void main(String args[]) throws Exception
+       public static void main(String args[]) throws Exception
     {
 
         Scanner sc = new Scanner(System.in);
         String A = sc.next();
-        String B = A.toUpperCase(Locale.ROOT);
+        String B = A.toUpperCase();
 
         System.out.println(B);
-        
+
     }
 }
