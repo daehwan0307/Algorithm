280a7681201eecb882ff9e8493c700c4b59894ee
DaeHwan Jung <50093472+daehwan0307@users.noreply.github.com>
2022-10-28 09:33:09 +0900

    [level 1] Title: 폰켓몬, Time: 3.34 ms, Memory: 81.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,16 @@
+import java.util.HashSet;
+import java.util.Set;
+class Solution {
+        public int solution(int[] nums) {
+            Set<Integer> set = new HashSet<>();
+            for (int i = 0; i < nums.length; i++) {
+                set.add(nums[i]);  //중복을 없애기 위해 set을 만듬
+            }
+            int N = nums.length/2;
+            
+            if(set.size()< N){
+                return set.size();
+            }else 
+                return N;
+        }
+    }
