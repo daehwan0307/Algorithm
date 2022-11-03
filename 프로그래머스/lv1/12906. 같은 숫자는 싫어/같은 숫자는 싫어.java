4e00aa72aae613f8645b030cb4b4de465a135776
DaeHwan Jung <50093472+daehwan0307@users.noreply.github.com>
2022-11-03 22:40:34 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 41.66 ms, Memory: 117 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+      
+        Stack<Integer> S = new Stack<>();
+        S.push(arr[0]);
+        for(int i=1;i<arr.length;i++){
+            if(S.peek()!=arr[i])S.push(arr[i]);
+            }
+        int[] answer =new int[S.size()];
+        for(int i=0;i<S.size();i++){
+            answer[i]=S.get(i);
+        }
+        return answer;
+    }
+}
