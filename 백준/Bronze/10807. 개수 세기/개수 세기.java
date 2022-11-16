623dbdb6a548e6d72518d515c6ce0172b3a05d18
DaeHwan Jung <50093472+daehwan0307@users.noreply.github.com>
2022-11-16 22:42:48 +0900

    [Bronze V] Title: 개수 세기, Time: 216 ms, Memory: 17816 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+    	
+    	int size = sc.nextInt();   	
+    	int[] numArr = new int[size];
+    	int count=0;
+    	for(int i =0;i<size;i++) {
+    		numArr[i]=sc.nextInt();
+    	}
+    	int a = sc.nextInt();
+    	for(int i =0;i<size;i++) {
+    		if(numArr[i]==a) {
+    			count++;
+    		}
+    	}
+    	System.out.print(count);
+    	
+    }
+}
