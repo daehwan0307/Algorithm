51da93b66798d9a784426c078852e75a444e80af
DaeHwan Jung <50093472+daehwan0307@users.noreply.github.com>
2022-11-17 22:35:52 +0900

    [Bronze III] Title: 직각삼각형, Time: 212 ms, Memory: 17804 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.java
@@ -0,0 +1,30 @@
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		while(true) {
+			int[] arr = new int[3];
+			for(int i = 0;i<3;i++) {
+				arr[i]=sc.nextInt();
+			}
+			Arrays.sort(arr);
+			if(arr[0]==0) {
+				break;
+			}
+			if(arr[0]*arr[0]+arr[1]*arr[1]==arr[2]*arr[2]) {
+				System.out.println("right");
+				
+			}else
+				System.out.println("wrong");
+			
+		}
+				
+	}
+
+}
