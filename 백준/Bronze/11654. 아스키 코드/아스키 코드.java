d310fc41034b557b22676210619eacd80baf449a
DaeHwan Jung <50093472+daehwan0307@users.noreply.github.com>
2022-11-17 09:24:30 +0900

    [Bronze V] Title: 아스키 코드, Time: 256 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in); 	
+    	char T = sc.nextLine().charAt(0);  
+    	int a = (int)T;
+    	System.out.println(a);
+
+    }
+}
