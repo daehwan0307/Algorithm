c86a5e44c8bcf9900af244e8ca582d3f6914cdcf
DaeHwan Jung <eoghks960307@naver.com>
2026-06-03 17:43:11 +0900

    [level 2] Title: 올바른 괄호, Time: 6.54 ms, Memory: 59.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+
+          Stack<Character> st = new Stack<>();
+        
+
+        
+        for(int i=0; i<s.length();i++){
+            if(s.charAt(i)=='('){
+                st.push(s.charAt(i));
+            }else
+            {
+                if(st.isEmpty()){
+                    return false;
+                }
+                st.pop();
+            }
+        }
+                
+        if(st.isEmpty()){
+            return true;
+        }
+       return false;
+    }
+}
